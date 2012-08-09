a859f49bb519f28ba14b6d7a4aefe4db074fea45
Scott Luther <scott@webgroupmedia.com>
2012-08-09 16:51:56 -0700

    Added NotificationTableCell.h and NotificationTableCell.m files so the build no longer breaks without them

---
+++ Cerb6 Notifications/NotificationTableCell.h
@@ -0,0 +1,13 @@
+//
+//  NotificationTableCell.h
+//  Cerb6 Notifications
+//
+//  Created by Scott on 8/9/12.
+//  Copyright (c) 2012 WebGroup Media. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NotificationTableCell : NSTableCellView
+
+@end
---
+++ Cerb6 Notifications/NotificationTableCell.m
@@ -0,0 +1,28 @@
+//
+//  NotificationTableCell.m
+//  Cerb6 Notifications
+//
+//  Created by Scott on 8/9/12.
+//  Copyright (c) 2012 WebGroup Media. All rights reserved.
+//
+
+#import "NotificationTableCell.h"
+
+@implementation NotificationTableCell
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
