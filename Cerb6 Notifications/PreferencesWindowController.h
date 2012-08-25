5a83f8d1cd069a47864b87f85c7a9dabaa0737f5
Scott Luther <scott@webgroupmedia.com>
2012-08-24 21:59:12 -0700

    You can now cancel adding a site
    Fixes #2

--- Cerb6 Notifications/PreferencesWindowController.h
+++ Cerb6 Notifications/PreferencesWindowController.h
@@ -7,12 +7,23 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "Site.h"
 
-@interface PreferencesWindowController : NSWindowController {
+@class AppDelegate;
+
+@interface PreferencesWindowController : NSWindowController <NSTableViewDataSource, NSTableViewDelegate> {
+	AppDelegate <NSApplicationDelegate> *appDelegate;
 	IBOutlet NSPanel *siteSheet;
 }
 
-- (void)controlTextDidChange:(NSNotification *)notification;
+//- (void)controlTextDidChange:(NSNotification *)notification;
+
+@property IBOutlet NSTableView *sitesTable;
+
+@property IBOutlet NSTextField *name;
+@property IBOutlet NSTextField *url;
+@property IBOutlet NSTextField *accessKey;
+@property IBOutlet NSTextField *secretKey;
 
 - (IBAction) closeSiteSheet:(id)sender;
 - (IBAction) showSiteSheet:(id)sender;
--- Cerb6 Notifications/PreferencesWindowController.m
+++ Cerb6 Notifications/PreferencesWindowController.m
@@ -7,13 +7,26 @@
 //
 
 #import "PreferencesWindowController.h"
+#import "AppDelegate.h"
 
 @implementation PreferencesWindowController
 
+@synthesize sitesTable;
+
+@synthesize name;
+@synthesize url;
+@synthesize accessKey;
+@synthesize secretKey;
+
 - (IBAction) closeSiteSheet:(id)sender
 {
-	[NSApp endSheet:siteSheet];
-	[siteSheet orderOut:sender];
+	[self closeSheet];
+}
+
+- (IBAction) saveSite:(id)sender
+{
+	
+	[self closeSheet];
 }
 
 - (IBAction) showSiteSheet:(id)sender
@@ -21,12 +34,25 @@
 	[NSApp beginSheet:siteSheet modalForWindow:[self window] modalDelegate:self didEndSelector:nil contextInfo:nil];
 }
 
-- (id)initWithWindow:(NSWindow *)window
+- (void) closeSheet {
+	[NSApp endSheet:siteSheet];
+	[siteSheet orderOut:self];
+}
+
+- (void) doubleClick:(id)sender
+{
+	NSInteger row = [sitesTable clickedRow];
+	
+	Site *site = [[appDelegate sites] objectAtIndex:row];
+	
+	// edit site
+}
+
 - (id) initWithWindowNibName:(NSString *)windowNibName
 {
 	self = [super initWithWindowNibName:windowNibName];
     if (self) {
-        // Initialization code here.
+		appDelegate = (AppDelegate *) [[NSApplication sharedApplication] delegate];
     }
 	return self;
 }
@@ -36,15 +62,49 @@
     [super windowDidLoad];
 
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+	[sitesTable reloadData];
 }
 
 - (void)controlTextDidChange:(NSNotification *)notification
 {
-//	NSLog(@"%@", notification);
-////	[portSelection selectCellAtRow:2 column:1];
-////	[[NSUserDefaults standardUserDefaults] setValue:@"2" forKey:@"portSelection"];
-//	
-//	
+
+}
+
+- (void)awakeFromNib
+{
+	[sitesTable setTarget:self];
+	[sitesTable setDoubleAction:@selector(doubleClick:)];
+}
+
+- (NSInteger) numberOfRowsInTableView:(NSTableView *)tableView
+{
+	NSLog(@"%@", [appDelegate sites]);
+	return [[appDelegate sites] count];
+}
+
+- (NSView *) tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+	
+	if([appDelegate sites] == nil) {
+		return nil;
+	}
+	
+	// Grab the column identifier
+	NSString *identifier = tableColumn.identifier;
+	NSTableCellView *cellView = [tableView makeViewWithIdentifier:identifier owner:self];
+	
+	Site *site = [[appDelegate sites] objectAtIndex:row];
+	
+	cellView.textField.stringValue = site.name;
+//	if([identifier isEqualToString:@"site"]) {
+//		Site *site = notification.site;
+//		cellView.textField.stringValue = [[NSString alloc] initWithFormat:@"%@", site.name];
+//	} else if([identifier isEqualToString:@"created"]) {
+//		cellView.textField.stringValue = [Utils prettySecs:notification.created];
+//	} else if ([identifier isEqualToString:@"message"]) {
+//		cellView.textField.stringValue = notification.message;
+//	}
+	
+	return cellView;
 }
 
 @end
